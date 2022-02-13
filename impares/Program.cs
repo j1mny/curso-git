f8ebb94d085caf57d3121a3cd30d0379db2ea905
Rafael Braciak <rafaelbcleite@hotmail.com>
2022-02-13 14:20:47 -0300

    Lendo um numero N, e depois mostrando todos os pares que existem ate ele

---
+++ impares/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace impares
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int X, intervalo;
+
+            X = int.Parse(Console.ReadLine());
+
+           for(int i = 1; i <= X; i = i + 2)
+            {
+
+                if(X % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+                else
+                {
+                    
+                    Console.WriteLine(i);
+                }  
+            }
+            
+        }
+    }
+}
